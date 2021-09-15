e854258d338879d76b25f445883f24602fc8e678
pichuzhkina <88-katerina@mail.ru>
2021-09-15 22:58:59 +0300

    name validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,51 @@
-'use strict';
-
-document.addEventListener("DOMContentLoaded", () => {
+$(document).ready(function() {
     
-	const name = document.getElementById('#name'),
-		  email = document.getElementById('#email'),
-		  phone = document.getElementById('#phone'),
-		  checkbox = document.querySelector('.form__checkbox'),
-		  radio = document.querySelector('.form__radio'),
-		  select = document.querySelector('.form__select'),
-		  file = document.querySelector('.form__file'),
-		  submit = document.getElementById('#submit');
+	// const name = $('#name'),
+	// 	  email = $('#email'),
+	// 	  phone = $('#phone'),
+	// 	  checkbox = $('.form__checkbox'),
+	// 	  radio = $('.form__radio'),
+	// 	  select = $('.form__select'),
+	// 	  file = $('.form__file');
 
-	
+
+	$('#name').on({
+		input: function() {
+			const val = this.value.replace(/[^a-zа-я _-]/gmi,'');
+			$(this).val(val);     
+		},
+		blur: function() {
+			const val = this.value.trim();
+			$(this).val(val);
+			checkLength(this);
+		}
+	});
+
+	function checkLength(name) {
+        let message = "Имя должно быть не короче 2 символов";
+        $('#name').addClass('error').after(`<span class='text-error'>${message}</span>`);
+    }
+
+	$('#email').on('blur', function() {
+		
+	});
+
+		  
+	$('#submit').on('submit', function() {
+		e.preventDefault();
+		console.log(111);
+
+		$.ajax({
+			type: 'POST',
+			url: 'request.php',
+			data: 1,
+			success: function(data) {
+				console.log(data);
+			},
+			error: function() {
+				console.log('error');
+			}
+		});
+	});
 
 });
