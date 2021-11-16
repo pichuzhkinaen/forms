3c5ea78a41a8fd259981db192430f3f907915958
pichuzhkina <88-katerina@mail.ru>
2021-11-16 23:43:31 +0300

    validate file and email

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -17,9 +17,20 @@ $(document).ready(function() {
 		checkLength(this.value.length, this);
 	});
 
+	$('#email').on('blur', function() {
+		checkEmail(this.value, this);
+	});
 
-	function checkLength(length, input) {
-        const msg = ((length === 1)) ? 'Имя должно быть не короче 2 символов' : '';
+	$("input[name='file']").on("change", function(e) {
+        e.preventDefault();
+ 
+        const fileInput = $("input[name='file']");
+        validateFile(fileInput, e);
+    });
+
+
+	function checkLength(inputLength, input) {
+        const msg = ((inputLength === 1)) ? 'Имя должно быть не короче 2 символов' : '';
 
 		if ($('.error-text').length === 0) {
 			$(input).after(`<span class="error-text">${msg}</span>`);
@@ -28,9 +39,46 @@ $(document).ready(function() {
         // input.setCustomValidity(msg); // Изменение браузерного сообщения, setCustomValidity работает только при отправке формы
     }
 
-	// $('#email').on('blur', function() {
-	// 	const pattern = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/gm;
-	// });
+	function checkEmail(inputValue, input) {
+		const pattern = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/gm;
+		if ( ! inputValue.match(pattern)) {
+			$(input).after(`<span class="error-text">Введите действительный адрес электронной почты</span>`);
+		} else {
+			$(input).siblings('.error-text').remove();
+		}
+		// console.log(inputValue.match(pattern));
+	}
+  
+    function validateFile(fileInput, e) {
+        $('.file-error').text('');
+ 
+        const sizeInput = fileInput[0].files[0].size,
+            nameFile = fileInput[0].files[0].name,
+            extensionFile = nameFile.substring(nameFile.lastIndexOf('.') + 1),
+            sizeMax = 20,
+            relType = ['doc', 'docx', 'pdf'];
+			
+        if (sizeInput > sizeMax * 1024 * 1024) {
+			$(fileInput).siblings('.error-text').remove();
+			$(fileInput).after(`<span class="error-text">Размер файла превышает ${sizeMax} Мбайт.</span>`);
+            return false;
+        }
+ 
+        let valid = false;
+ 
+        for (let i = 0; i < relType.length; i++) {
+            if (extensionFile === relType[i]) {
+                valid = true;
+            }
+        }
+        if (valid) {
+			$(fileInput).siblings('.error-text').remove();
+        } else {
+            // $(fileInput).val('');
+            $(fileInput).siblings('.error-text').remove();
+            $(fileInput).after(`<span class="error-text">Формат файла не поддерживается, допустимые форматы: Word и PDF</span>`);
+        }
+    }
 		 
 	//Отправка формы
 	$('#submit').on('submit', function() {
